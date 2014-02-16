8b3b019af0c6bcd9a4973fc3757730d6192dbace
Nick Gaulin <ndgaulin@gmail.com>
2014-02-16 06:18:18 -0600

    Update Console.js

--- Console.js
+++ Console.js
@@ -2,7 +2,7 @@
 //                         jTypes Development Studio
 //  ------------------------------------------------------------------------
 //
-//                   Copyright 2013 Gaulinsoft Corporation
+//                   Copyright 2014 Gaulinsoft Corporation
 //
 //       Licensed under the Apache License, Version 2.0 (the "License");
 //      you may not use this file except in compliance with the License.
